3801adfd29ad7343d89869261e13dff0c370919f
Nicolas Budyn <nbud1@lycos.com>
2017-03-03 12:19:54 +0000

    Transmission-reflection coefficients: add example of solid-to-liquid interface and update energy conservation equations

--- tests/test_ut.py
+++ tests/test_ut.py
@@ -170,6 +170,8 @@ def test_fluid_solid_real():
     """
     Test fluid_solid() below critical angles (real only).
     The conservation of energy should be respected.
+
+    Stay below critical angles.
     """
     alpha_fluid = np.deg2rad([0, 5, 10])
 
@@ -192,9 +194,14 @@ def test_fluid_solid_real():
                                                                     c_fluid, c_l,
                                                                     c_t,
                                                                     alpha_l, alpha_t)
+    assert reflection.dtype.kind == 'f'
+    assert transmission_l.dtype.kind == 'f'
+    assert transmission_t.dtype.kind == 'f'
 
-    # Compute the energy incoming and outcoming: the principle of conservation of energy must be respected.
+    # Compute the energy incoming and outcoming: the principle of conservation of energy
+    # must be respected.
     # Reference: Schmerr, §6.3.2
+    # Caveat about inhomogeneous waves: Schmerr, §6.2.5
 
     # incident pressure
     pres_i = 10000.
@@ -210,7 +217,6 @@ def test_fluid_solid_real():
     energy_l = 0.5 * (transmission_l * pres_i) ** 2 / (rho_solid * c_l) * area_l
     energy_t = 0.5 * (transmission_t * pres_i) ** 2 / (rho_solid * c_t) * area_t
 
-    # Check equality of complex values (this equality has NO physical meaning)
     np.testing.assert_allclose(inc_energy, energy_refl + energy_l + energy_t)
 
 
@@ -242,47 +248,26 @@ def test_fluid_solid_complex():
                                                                     c_t,
                                                                     alpha_l, alpha_t)
 
-    # Compute the energy incoming and outcoming: the principle of conservation of energy must be respected.
+    # Compute the energy incoming and outcoming: the principle of conservation of energy
+    # must be respected.
     # Reference: Schmerr, §6.3.2
+    # Caveat about inhomogeneous waves: Schmerr, §6.2.5
 
     # incident pressure
     pres_i = 10000.
 
     # cross section areas:
-    area_fluid = np.cos(alpha_fluid)
-    area_l = np.cos(alpha_l)
-    area_t = np.cos(alpha_t)
-
-    critical_l = np.arcsin(c_fluid / c_l)
-    critical_t = np.arcsin(c_fluid / c_t)
-    case1 = alpha_fluid < critical_l
-    case2 = np.logical_and(critical_l < alpha_fluid, alpha_fluid < critical_t)
-    case3 = critical_t < alpha_fluid
-    assert np.any(case1)
-    assert np.any(case2)
-    assert np.any(case3)
+    area_fluid = np.cos(alpha_fluid.real)
+    area_l = np.cos(alpha_l.real)
+    area_t = np.cos(alpha_t.real)
 
     inc_energy = 0.5 * pres_i ** 2 / (rho_fluid * c_fluid) * area_fluid
-    energy_refl = 0.5 * (reflection * pres_i) ** 2 / (rho_fluid * c_fluid) * area_fluid
-    energy_l = 0.5 * (transmission_l * pres_i) ** 2 / (rho_solid * c_l) * area_l
-    energy_t = 0.5 * (transmission_t * pres_i) ** 2 / (rho_solid * c_t) * area_t
+    energy_refl = 0.5 * (np.abs(reflection) * pres_i) ** 2 / (
+        rho_fluid * c_fluid) * area_fluid
+    energy_l = 0.5 * (np.abs(transmission_l) * pres_i) ** 2 / (rho_solid * c_l) * area_l
+    energy_t = 0.5 * (np.abs(transmission_t) * pres_i) ** 2 / (rho_solid * c_t) * area_t
 
-    # Check equality of complex values (this equality has NO physical meaning)
     np.testing.assert_allclose(inc_energy, energy_refl + energy_l + energy_t)
-
-    # Check the conservation of energy for all cases:
-
-    # CASE 1 - Below the two critical angle
-    np.testing.assert_allclose(np.abs(inc_energy[case1]),
-                               np.abs(energy_refl[case1]) + np.abs(
-                                   energy_l[case1]) + np.abs(energy_t[case1]))
-
-    # CASE 2 - L reflected, T transmitted
-    np.testing.assert_allclose(np.abs(inc_energy[case2]),
-                               np.abs(energy_refl[case2]) + np.abs(energy_t[case2]))
-
-    # CASE 3 - L and T reflected
-    np.testing.assert_allclose(np.abs(inc_energy[case3]), np.abs(energy_refl[case3]))
 
 
 def test_solid_l_fluid():
@@ -313,8 +298,10 @@ def test_solid_l_fluid():
                                                                     alpha_fluid,
                                                                     alpha_t)
 
-    # Compute the energy incoming and outcoming: the principle of conservation of energy must be respected.
+    # Compute the energy incoming and outcoming: the principle of conservation of energy
+    # must be respected.
     # Reference: Schmerr, §6.3.2
+    # Caveat about inhomogeneous waves: Schmerr, §6.2.5
 
     # incident pressure
     pres_i = 10000.
@@ -361,41 +348,27 @@ def test_solid_t_fluid_complex():
                                                                     alpha_fluid,
                                                                     alpha_l)
 
-    # Compute the energy incoming and outcoming: the principle of conservation of energy must be respected.
+    # Compute the energy incoming and outcoming: the principle of conservation of energy
+    # must be respected.
     # Reference: Schmerr, §6.3.2
+    # Caveat about inhomogeneous waves: Schmerr, §6.2.5
 
     # incident pressure
     pres_i = 10000.
 
     # cross section areas:
-    area_fluid = np.cos(alpha_fluid)
-    area_l = np.cos(alpha_l)
-    area_t = np.cos(alpha_t)
-
-    critical = np.arcsin(c_t / c_l)
-    case1 = alpha_t < critical
-    case2 = np.logical_not(case1)
-    assert np.any(case1)
-    assert np.any(case2)
+    area_fluid = np.cos(alpha_fluid.real)
+    area_l = np.cos(alpha_l.real)
+    area_t = np.cos(alpha_t.real)
 
     inc_energy = 0.5 * pres_i ** 2 / (rho_solid * c_t) * area_t
-    energy_trans = 0.5 * (transmission * pres_i) ** 2 / (rho_fluid * c_fluid) * area_fluid
-    energy_l = 0.5 * (reflection_l * pres_i) ** 2 / (rho_solid * c_l) * area_l
-    energy_t = 0.5 * (reflection_t * pres_i) ** 2 / (rho_solid * c_t) * area_t
+    energy_trans = 0.5 * (np.abs(transmission) * pres_i) ** 2 / (
+    rho_fluid * c_fluid) * area_fluid
+    energy_l = 0.5 * (np.abs(reflection_l) * pres_i) ** 2 / (rho_solid * c_l) * area_l
+    energy_t = 0.5 * (np.abs(reflection_t) * pres_i) ** 2 / (rho_solid * c_t) * area_t
 
     # Check equality of complex values (this equality has NO physical meaning)
     np.testing.assert_allclose(inc_energy, energy_trans + energy_l + energy_t)
-
-    # Check the conservation of energy for all cases:
-
-    # CASE 1 - L is reflected
-    np.testing.assert_allclose(np.abs(inc_energy[case1]),
-                               np.abs(energy_trans[case1]) + np.abs(
-                                   energy_l[case1]) + np.abs(energy_t[case1]))
-
-    # CASE 2 - L not reflected
-    np.testing.assert_allclose(np.abs(inc_energy[case2]),
-                               np.abs(energy_trans[case2]) + np.abs(energy_t[case2]))
 
 
 def test_snell_angles():
