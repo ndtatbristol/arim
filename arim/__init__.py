f453680ed6197262c20c0eed04d1892fbfcc5d15
Nicolas Budyn <nbud1@lycos.com>
2018-03-13 10:57:54 +0000

    update version

--- arim/__init__.py
+++ arim/__init__.py
@@ -21,8 +21,8 @@ T = Mode.T
 __author__ = "Nicolas Budyn"
 __credits__ = []
 __license__ = "All rights reserved"
-__copyright__ = "2016, 2017, Nicolas Budyn"
+__copyright__ = "2016-2018, Nicolas Budyn"
 
 # Must respect PEP 440: https://www.python.org/dev/peps/pep-0440/
 # Must be bumped at each release
-__version__ = '0.8.dev1'
+__version__ = '0.8.dev2'
