b8065496d94a0699daceca75bcb820cbf872f1fa
Nicolas Budyn <nbud1@lycos.com>
2019-03-19 18:24:24 +0000

    add probe ima_100_MHz_128_1d

--- arim/_probes/bristol_ndt.py
+++ arim/_probes/bristol_ndt.py
@@ -191,3 +191,49 @@ makers.append(
         "Sonaxis 15.0 MHz 110 elts linear array probe",
     )
 )
+
+
+def _make_ima_100_MHz_128_1d():
+    metadata = dict(
+        probe_type="linear",
+        short_name="ima_100_MHz_128_1d",
+        long_name="Imasonic 10.0 MHz 128 elts linear array probe",
+        version=0,
+        serial="10345 A101",
+    )
+    numelements = 128
+    shapes = np.full(numelements, core.ElementShape.rectangular, dtype=np.object)
+    orientations = g.Points.from_xyz(
+        np.zeros((numelements,), dtype=np.float),
+        np.zeros((numelements,), dtype=np.float),
+        np.ones((numelements,), dtype=np.float),
+    )
+    dimensions = g.Points.from_xyz(
+        np.full(numelements, 0.25e-3),
+        np.full(numelements, 10e-3),
+        np.full(numelements, 0.0),
+    )
+    dead_elements = np.full(numelements, False, dtype=np.bool)
+
+    probe = core.Probe.make_matrix_probe(
+        numx=numelements,
+        pitch_x=0.3e-3,
+        numy=1,
+        pitch_y=np.nan,
+        frequency=10e6,
+        shapes=shapes,
+        orientations=orientations,
+        dimensions=dimensions,
+        dead_elements=dead_elements,
+        metadata=metadata,
+    )
+    return probe
+
+
+makers.append(
+    ProbeMaker(
+        _make_ima_100_MHz_128_1d,
+        "ima_100_MHz_128_1d",
+        "Imasonic 10.0 MHz 128 elts linear array probe",
+    )
+)
