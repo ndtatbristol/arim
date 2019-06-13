971360d3afcf7f28ec474785b608bfb914cc95ea
Nicolas Budyn <n.budyn@pm.me>
2019-06-13 14:10:20 +0100

    Update package requirements for dev install

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ setup(
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-    extras_require={"test": ["pytest"]},
+    extras_require={"dev": ["pytest", "numdifftools", "sphinx", "sphinx_rtd_theme"]},
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
