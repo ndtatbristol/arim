05dc91051826092e97e9f1ff7c2715d92ac1512e
Nicolas Budyn <n.budyn@pm.me>
2020-03-08 10:03:16 +0100

    Delete hardly used conda recipe

--- bld.bat
+++
@@ -1,5 +0,0 @@
-"%PYTHON%" setup.py install
-if errorlevel 1 exit 1
-
-"%PYTHON%" setup.py --version > __conda_version__.txt
-if errorlevel 1 exit 1
--- build.sh
+++
@@ -1,2 +0,0 @@
-$PYTHON setup.py install     # Python command to install the script.
-$PYTHON setup.py --version > __conda_version__.txt
