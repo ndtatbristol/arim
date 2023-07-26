1c79541a37b5b99b13a0c18d021fd20f828e7581
Matt Chandler <mc16535@bristol.ac.uk>
2023-07-26 13:37:50 +0100

    changed to environment variable rather than global variable, and removed dependence from arim/config.py. Default behaviour goes to parallel=True, so set environment variable `ARIM_USE_PARALLEL` to `False` to disable.

--- arim/im/geomed.py
+++ arim/im/geomed.py
@@ -5,10 +5,11 @@ Geometric median of 2D points using Newton's descent with backtracking line-sear
 import numba
 import math
 import numpy as np
-from ..config import USE_PARALLEL
+import os
 
 # enable SIMD!
-_numba_opts = dict(nogil=True, parallel=USE_PARALLEL, fastmath=True, error_model="numpy")
+use_parallel = os.environ.get("ARIM_USE_PARALLEL", True)
+_numba_opts = dict(nogil=True, parallel=use_parallel, fastmath=True, error_model="numpy")
 
 
 @numba.njit(**_numba_opts)
