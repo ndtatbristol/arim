8ba731ec6a0d6dec7df9cf007bbef010a24ecbc0
Matt Chandler <matt@mgchandler.com>
2024-07-26 16:31:09 +0100

    Added new data set to datasets.py, and updated the base_url. I haven't used pooch before so I hope this is right!

--- src/arim/datasets.py
+++ src/arim/datasets.py
@@ -14,12 +14,13 @@ import pooch
 
 EXAMPLES = pooch.create(
     path=pooch.os_cache("arim"),
-    base_url="https://github.com/ndtatbristol/arim/raw/25391358a02d121c6fe0f3114e193606e581c8b9/examples/example-datasets/",
+    base_url="https://github.com/ndtatbristol/arim/raw/2b419534304b1edc4119ec734006f380b772742a/examples/example-datasets/",
     version=None,
     version_dev=None,
     registry={
         "contact_notch_aluminium.mat": "sha256:c58d25ccaf3081348c392d98a45a5e28809c80c900782a9b60f62d1a7ef2c76f",
         "immersion_notch_aluminium.mat": "sha256:ae125a6c461a10ded54ca19e223c4d39fe857be8c12fda6f97b61de25a52a08e",
+        "contact_sdh_aluminium_nlos.mat": "sha256:f66df7ddfc236222b3fdc35a6cd4247661474b03d3918fdb4c1ec63752e6f0c0",
     },
 )
 
